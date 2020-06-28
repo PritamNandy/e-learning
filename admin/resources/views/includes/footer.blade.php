12c3947765b76171536f21ab23436061dbd39df4
Prasun Nandy Pritam <prasun.nandy@northsouth.edu>
2020-06-28 18:29:57 +0600

    First Commit
    First Commit

---
+++ admin/resources/views/includes/footer.blade.php
@@ -0,0 +1,25 @@
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; {{date('Y')}} <a href="https://heydev.net">HeyDev.net</a>.</strong> All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('assets/dist/js/adminlte.min.js')}}"></script>
+</body>
+</html>
---
+++ site/resources/views/includes/header.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="bn">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title') | e-Learning | Online Learning Platform</title>
+    <meta name="description" content="I am working as a web developer for more that 3 years. I have professional expertise in Laravel, Codeignter, PHP, HTML5, CSS3, JavaScript ES6, jQuery, Bootstrap, Ajax, React.">
+    <meta name="keywords" content="Web Developer, Web Developer in Bangldesh, Laravel Developer, Codeigniter Developer, Full stack web developer, React Developer">
+    <meta name="author" content="Prasun Nandy Pritam">
+    <meta name="link" content="heydev.net">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/custom.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/owl.carousel.min.css')}}" rel="stylesheet" >
+    <link href="{{asset('css/fontawesome.css')}}" rel="stylesheet">
+    <link href="{{asset('css/animate.css')}}" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar fixed-top nav-before navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img class="nav-logo" src="images/navlogo.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-3 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link nav-font" href="#">হোম </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font" href="courses.html">কোর্স সমুহ </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font" href="projects.html">প্রোজেক্ট </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font" href="blog.html">ব্লগ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-font" href="#">যোগাযোগ</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <button class="normal-btn btn" >সাইন ইন</button>
+        </form>
+    </div>
+</nav>
